c0536b82bce4bb5d6181614405416dbc26d60085
Margie Cao <mcao50@stuy.edu>
2024-09-17 11:47:49 -0400

    started classwork

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,15 @@
+# Moyo Fagbuyi, Margie Cao, Tim Ng
+# Systems Level Programming
+# SoftDev
+# K04 -- Python dictionairies and random selection
+# 2024-09-13
+# time spent: 0.6
+
+import random
+
+def duckies(str):
+    periods = str.split("@@@")
+    
+    
+x = open("krewes.txt", "r")
+duckies(x)
