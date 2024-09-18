9114814726253e2641eea992e92dbc069ee21a2b
Margie Cao <mcao50@stuy.edu>
2024-09-18 02:16:32 -0400

    finished making list of dictionaries and randomly selecting a devo

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,15 +1,27 @@
-# Moyo Fagbuyi, Margie Cao, Tim Ng
+# Moyo Fagbuyi, Tim Ng, Margie Cao
 # Systems Level Programming
 # SoftDev
-# K04 -- Python dictionairies and random selection
-# 2024-09-13
-# time spent: 0.6
+# K06 -- Working with str.split(), lists, and dictionaries
+# 2024-09-17
+# time spent: 1
 
 import random
 
 def duckies(str):
-    periods = str.split("@@@")
-    
+    devos = str.split("@@@")
+    devos.remove("")
+    info = []
+    listdict = []
+    for x in range(len(devos)):
+        info = info + devos[x].split("$$$")
+    for x in range(0, len(info), 3):
+        dict = {"pd": 1, "devo": "temp", "ducky": "temp"}
+        dict["pd"] = info[x]
+        dict["devo"] = info[x+1]
+        dict["ducky"] = info[x+2]
+        listdict.append(dict)
+    x = random.randint(0, len(listdict)-1)
+    print(listdict[x]["devo"] + " " + listdict[x]["pd"] + " " + listdict[x]["ducky"])
     
 x = open("krewes.txt", "r")
-duckies(x)
+duckies(x.read())
