3b72d4b86ae9d6d756e5128424b1c900522a1bbb
Margie Cao <mcao50@stuy.edu>
2024-09-26 11:47:59 -0400

    classwork

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -1,11 +1,12 @@
-# Clyde 'Thluffy' Sinclair
+# Margie, Tim, Moyo
 # SoftDev
-# Sep 2024
+# Sep 26 2024
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Q0:
-<your response here>
+P: When you remove render_template, we will get an error since render_template is undefined
+A: We get a NameError since name 'render_template' is not defined
 
 Q1:
 <your response here>
@@ -21,7 +22,7 @@ Q2:
 # Q0: What will happen if you remove render_template from the following statement?
 # (log prediction before executing...)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
