2d7e9585d93f5396da61c1f967c12117a023c825
Margie Cao <mcao50@stuy.edu>
2024-10-09 10:48:03 -0400

    committing from last class

---
+++ 15_flask-forms/app.py
@@ -0,0 +1,25 @@
+'''
+Margie Cao, Moyo Fagbuyi
+M&M
+SoftDev
+K15 -- Creating a Flask App
+2024-10-08
+Time Spent: #hours
+'''
+    
+from flask import Flask, render_template # render_template is an import from flask
+app = Flask(__name__) # assigns the Flask constructor to variable app
+
+@app.route("/") # home route
+def starting_page():
+
+@app.route("/login") 
+def occupations():
+    
+
+@app.route("/response")
+
+
+if __name__ == "__main__": 
+    app.debug = True # allows for constant refreshing
+    app.run() # runs the app
