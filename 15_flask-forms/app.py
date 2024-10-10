b9f1c66ae89f46ea1c6805c718e8a9ef61f7b29f
Margie Cao <mcao50@stuy.edu>
2024-10-10 02:33:48 -0400

    finished k15 notes & display

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -4,21 +4,25 @@ M&M
 SoftDev
 K15 -- Creating a Flask App
 2024-10-08
-Time Spent: #hours
+Time Spent: 2 hours
 '''
     
-from flask import Flask, render_template # render_template is an import from flask
-app = Flask(__name__) # assigns the Flask constructor to variable app
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-@app.route("/") # home route
+@app.route("/", methods=['GET', 'POST']) # home route
 def starting_page():
-
-@app.route("/login") 
-def occupations():
+    return render_template("login.html")
     
-
-@app.route("/response")
-
+@app.route("/response", methods=['GET', 'POST'])
+def response_page():
+    request = request.method
+    if request.method == "GET": 
+        username = request.args["username"] #taking data from args library
+    elif request.method == "POST":
+        username = request.form["username"] #different from get method, stores in form library
+    breakdown = "the GET method is HTML's default method and checks the args dictionary for the data inputted. This data can be seen in the URL unlike the POST method which stores the data in a form dictionary by sending it to the server."        
+    return render_template("response.html", request = request, username = username, breakdown = breakdown)
 
 if __name__ == "__main__": 
     app.debug = True # allows for constant refreshing
