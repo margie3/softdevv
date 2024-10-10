087b22607949eb52a526c703947a879f73d14a24
Margie Cao <mcao50@stuy.edu>
2024-10-10 11:47:39 -0400

    working on making sessions and cookies

---
+++ 16_flask-sessions/app.py
@@ -0,0 +1,30 @@
+'''
+Margie Cao, Moyo Fagbuyi
+M&M
+SoftDev
+K15 -- Creating a Flask App
+2024-10-08
+Time Spent: 2 hours
+'''
+    
+from flask import Flask, render_template, request, session
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST']) # home route
+def starting_page():
+    return render_template("login.html")
+    
+@app.route("/auth", methods=['GET', 'POST'])
+def response_page():
+    requested = request.method
+    print(request.method)
+    if request.method == 'GET': 
+        username = request.args["username"] #taking data from args library
+    elif request.method == 'POST':
+        username = request.form["username"] #different from get method, stores in form library
+    breakdown = "the GET method is HTML's default method and checks the args dictionary for the data inputted. This data can be seen in the URL unlike the POST method which stores the data in a form dictionary by sending it to the server."        
+    return render_template("response.html", request = requested, username = username, breakdown = breakdown)
+
+if __name__ == "__main__": 
+    app.debug = True # allows for constant refreshing
+    app.run() # runs the app
---
+++ 16_flask-sessions/utl/ants.py
@@ -0,0 +1,4 @@
+import os
+
+def sessionKey:
+    return os.urandom(32)
