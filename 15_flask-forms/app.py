079bd6219f5f754006d00b5b71a1429def514cea
Margie Cao <mcao50@stuy.edu>
2024-10-10 11:24:56 -0400

    fixed response pathway

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -14,15 +14,16 @@ app = Flask(__name__)
 def starting_page():
     return render_template("login.html")
     
-@app.route("/response", methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def response_page():
-    request = request.method
-    if request.method == "GET": 
+    requested = request.method
+    print(request.method)
+    if request.method == 'GET': 
         username = request.args["username"] #taking data from args library
-    elif request.method == "POST":
+    elif request.method == 'POST':
         username = request.form["username"] #different from get method, stores in form library
     breakdown = "the GET method is HTML's default method and checks the args dictionary for the data inputted. This data can be seen in the URL unlike the POST method which stores the data in a form dictionary by sending it to the server."        
-    return render_template("response.html", request = request, username = username, breakdown = breakdown)
+    return render_template("response.html", request = requested, username = username, breakdown = breakdown)
 
 if __name__ == "__main__": 
     app.debug = True # allows for constant refreshing
