09be746bd35c0bbf28c27fe001f6924dfcb21268
Margie Cao <mcao50@stuy.edu>
2024-10-15 11:01:20 -0400

    finished 16_flask-sessions

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,30 +1,42 @@
-'''
-Margie Cao, Moyo Fagbuyi
-M&M
-SoftDev
-K15 -- Creating a Flask App
-2024-10-08
-Time Spent: 2 hours
-'''
-    
-from flask import Flask, render_template, request, session
+# Margie Cao, Moyo Fagbuyi, Tim Ng
+# System Level Programming
+# K16 - working with sessions and cookies
+# SoftDev
+# 2024-10-09
+# Time Spent: 3 hours
+
+from flask import Flask, render_template, request, session #this one stores like verything
+import os 
+from utl import ants
+
 app = Flask(__name__)
+app.secret_key = ants.poop()
 
-@app.route("/", methods=['GET', 'POST']) # home route
-def starting_page():
-    return render_template("login.html")
-    
-@app.route("/auth", methods=['GET', 'POST'])
-def response_page():
-    requested = request.method
-    print(request.method)
-    if request.method == 'GET': 
-        username = request.args["username"] #taking data from args library
-    elif request.method == 'POST':
-        username = request.form["username"] #different from get method, stores in form library
-    breakdown = "the GET method is HTML's default method and checks the args dictionary for the data inputted. This data can be seen in the URL unlike the POST method which stores the data in a form dictionary by sending it to the server."        
-    return render_template("response.html", request = requested, username = username, breakdown = breakdown)
+@app.route("/",  methods=['GET','POST'])
+def disp_loginpage():
+    if 'username' and 'password' in session: # will immediately send you to response page if already logged
+        return render_template('response.html')
+    return render_template( 'login.html' ) #renders homepage
 
-if __name__ == "__main__": 
-    app.debug = True # allows for constant refreshing
-    app.run() # runs the app
+
+@app.route("/auth", methods=['POST'])
+def post():
+    session['username'] = request.form['username'] # saves username + password
+    session['password'] = request.form['password']
+    name = session['username']
+    return render_template('response.html', name = name)
+
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    session.pop('username', None) # deletes username and password from cookies
+    session.pop('password', None)
+    return render_template('logout.html')
+
+@app.route("/homepage", methods = ['GET', 'POST'])
+def redirect():
+    return render_template('login.html')
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
